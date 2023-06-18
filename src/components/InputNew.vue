e2b295b8cbde76f9ae0849af0c2f30940543d17f
Damián Cano López <damiancano1811@hotmail.com>
2023-06-18 14:43:50 -0500

    New validations, to improve the addition of tasks

--- src/components/InputNew.vue
+++ src/components/InputNew.vue
@@ -5,13 +5,17 @@ const text = ref("");
 const emits = defineEmits(["onNewItem"]);
 
 function handleKeyDown(evt) {
-  if (evt.key === "Enter") {
-    emits("onNewItem", text);
-    text.value = "";
+  if(text.value != ""){
+    if (evt.key === "Enter") {
+      emits("onNewItem", text);
+      text.value = "";
+    }
+  }else{
+    alert("It is necessary to enter the name of the task to be able to be added 😣")
   }
 }
 </script>
 
-<template><input v-model="text" @keydown="handleKeyDown" /></template>
+<template><input v-model="text" @keydown="handleKeyDown" placeholder="Add new task" /></template>
 
 <style scoped></style>
